70d0686b61e2846a10800e40fd6200e407713c31
Ståle Tomten <stale.tomten@finn.no>
2024-02-23 15:24:31 +0100

    Fix main.

--- internal/integration-test/src/main.ts
+++ internal/integration-test/src/main.ts
@@ -1,4 +1,4 @@
-import createServer from './index';
+import createServer from './index.js';
 
 const server = await createServer({ port: 3000 }).finally(() => {
     console.log('Server is running');
